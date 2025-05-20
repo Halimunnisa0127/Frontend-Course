8e3e5b421f509f2513c6b8abf166ab7c66f711d3
Munni <shaikmunni1314@gmail.com>
2025-05-20 15:10:23 +0530

    "Added basic movie booking system"

---
+++ Day-7/movies.js
@@ -0,0 +1,74 @@
+// Theater Data
+const cinema = {
+  name: "AMB Cinemas",
+  location: "HiTech City",
+  movies: [
+    {
+      title: "Kalki",
+      jonor: "Crime/Mystery/Thriller",
+      CBFC: "U/A/Hindi",
+      image: "https://media5.bollywoodhungama.in/wp-content/uploads/2024/06/Kalki-2898-AD-10-322x483.jpg"
+    },
+    {
+      title: "Pushpa 2",
+      jonor: "Action/Thriller",
+      CBFC: "U/A/Telugu",
+      image: "https://static.toiimg.com/photo/msid-115989021/115989021.jpg?433878"
+    },
+    {
+      title: "Salaar",
+      jonor: "Action/Thriller",
+      CBFC: "A/Telugu",
+      image: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/salaar-cease-fire--part-1-et00301886-1702971289.jpg"
+    },
+    {
+      title: "HIT: The Third Case",
+      jonor: "Crime/Mystery/Thriller",
+      CBFC: "A/Telugu",
+      image: "https://upload.wikimedia.org/wikipedia/en/thumb/0/09/HIT_-_The_Third_Case.jpg/250px-HIT_-_The_Third_Case.jpg"
+    },
+    {
+      title: "Subham",
+      jonor: "Comedy/Horror",
+      CBFC: "U/A/Telugu",
+      image: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/subham-et00440249-1747030168.jpg"
+    },
+    {
+      title: "Kesari Chapter 2",
+      jonor: "Action/rama/Political",
+      CBFC: "A/Hindi",
+      image: "https://assetscdn1.paytm.com/images/cinema/Kesari-2_Poster-470c3d50-0c8a-11f0-9fdc-bd083db259c1.jpg"
+    },
+    {
+      title: "Snow White",
+      jonor: "Action/rama/Political",
+      CBFC: "U/English",
+      image: "https://assetscdn1.paytm.com/images/cinema/Snow-white4-1cf829d0-032e-11f0-abed-03538c2be683.jpg?format=webp&imwidth=256"
+    },
+    {
+      title: "Final Destination Bloodlines",
+      jonor: "Horror/Supernatural/Thriller",
+      CBFC: "A/English",
+      image: "https://assetscdn1.paytm.com/images/cinema/FD4-58568940-2f2f-11f0-bd90-d94369f6f4ca.jpg"
+    },
+    {
+      title: "Phule",
+      jonor: "Biography",
+      CBFC: "U/Hindi",
+      image: "https://assetscdn1.paytm.com/images/cinema/POSTERS_0003_remove%20the%20text%2019-b1fcca60-139e-11f0-8aed-f941662e2d49.jpg"
+    },
+    {
+      title: "Lovely",
+      jonor: "Comedy/Fantasy",
+      CBFC: "UA+13/Hindi",
+      image: "https://assets.gadgets360cdn.com/pricee/assets/product/202503/Lovely_poster_2_1743155239.jpg"
+    },
+    {
+      title: "Paddington In Peru",
+      jonor: "Animation/Advanture/Family/Mystery",
+      CBFC: "U/Hindi/English",
+      image: "https://assetscdn1.paytm.com/images/cinema/PADDINGTON-IN-PERU2-25983c20-fb3f-11ef-8434-f9ed39b3241b.jpg"
+    },
+
+  ]
+};
